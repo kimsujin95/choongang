336dcb5577c95cf9f6e3a3cdb78f3d76380e4a14
kimsujin95 <106326854+kimsujin95@users.noreply.github.com>
2023-06-15 18:37:23 +0900

    Delete src directory

--- src/zoo/Cat.java
+++
@@ -1,6 +0,0 @@
-package zoo;
-
-public class Cat extends Ancestor {
-	public String name;
-	public int age;
-}
--- src/zoo/Dog.java
+++
@@ -1,6 +0,0 @@
-package zoo;
-
-public class Dog extends Ancestor {
-	public String name;
-	public int age;
-}
